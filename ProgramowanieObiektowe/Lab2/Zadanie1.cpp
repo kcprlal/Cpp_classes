a6673bed83152c3844b19937a935f3433bcce89d
Kacper Lalik <kacper.lalik@interia.pl>
2024-10-18 10:38:11 +0200

    kom

--- ProgramowanieObiektowe/Lab2/Zadanie1.cpp
+++ ProgramowanieObiektowe/Lab2/Zadanie1.cpp
@@ -2,6 +2,26 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+
+int sum_matrix(const std::vector<std::vector<int>> &col_matrix)
+{
+    int sum;
+    for (int i{}; const auto &col_element : col_matrix)
+    {
+        for (int j{}; const auto &row_element : col_element)
+        {
+            if (i % 2 != 0 || j % 2 == 0)
+            {
+                sum += row_element;
+            }
+
+            j++;
+        }
+        i++;
+    }
+    return sum;
+}
+
 int main()
 {
     srand(static_cast<unsigned int>(time(0)));
@@ -9,10 +29,10 @@ int main()
     std::vector<int> matrix_rows{};
     std::vector<std::vector<int>> matrix_cols{};
     int row, col, sum;
-    bool evenRow = true, everySecondElement = true;
+
     std::cout << "Pick number of rows and cols: ";
     std::cin >> row >> col;
-    
+
     for (int j{}; j < col; ++j)
     {
         for (int i{}; i < row; ++i)
@@ -28,22 +48,12 @@ int main()
         for (int j{}; const auto &row_element : col_element)
         {
             std::cout << row_element << " ";
-            if (i % 2 == 0)
-            {
-                if (j % 2 == 0)
-                {
-                    sum += row_element;
-                }
-            }
-            else
-            {
-                sum += row_element;
-            }
             j++;
         }
         std::cout << std::endl;
         i++;
     }
+    sum=sum_matrix(matrix_cols);
     std::cout << sum << std::endl;
 
     return 0;
--- ProgramowanieObiektowe/Lab2/Zadanie2.cpp
+++ ProgramowanieObiektowe/Lab2/Zadanie2.cpp
@@ -1,14 +1,19 @@
-#include<iostream>
+#include <iostream>
 #include <string>
-#include<algorithm>
-int main(){
-    std::string word{"let's go"}, temp{};
+#include <algorithm>
+int main()
+{
+    std::string word{}, temp{};
     int i{};
-    for(int i = word.size()-1; i>=0; --i){  
-        temp+=word[i];
+
+    std::cin >> word;
+
+    for (int i = word.size() - 1; i >= 0; --i)
+    {
+        temp += word[i];
     }
-    std::cout<<temp<<std::endl;
+    std::cout << "rotated manually: " << temp << std::endl;
     std::reverse(word.begin(), word.end());
-    std::cout<<word<<std::endl;
+    std::cout << "rotated by function: " << word << std::endl;
     return 0;
 }
